62ddef29ba51def1293eb762f118db4d9618e724
Satyaranjan Muduli <Satyaranjan.Muduli@mea.ey.net>
2018-07-07 11:20:20 +0530

    Uploading DemoAppiumAPP Project

---
+++ src/com/demo/googleplay/GooglePlayTest.java
@@ -0,0 +1,90 @@
+package com.demo.googleplay;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
+
+public class GooglePlayTest 
+{
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() throws MalformedURLException 
+	{
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability(CapabilityType.VERSION, "5.1.1");
+		capabilities.setCapability(MobileCapabilityType.NO_RESET, true); //Won't start from the scratch without reseting the data.
+		capabilities.setCapability("deviceName", "92010457a0ce2315");
+		capabilities.setCapability("appPackage", "com.android.vending");
+		capabilities.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	
+	@Test(priority=2)
+	public void searchPlayStore() throws InterruptedException 
+	{
+		
+		
+		Thread.sleep(3000);
+		//MobileElement element = (MobileElement) driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(new UiSelector().description(\"Search\"))"));
+		driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Search']")).click();
+		//element.click();
+		driver.findElement(By.className("android.widget.EditText")).sendKeys("flipkart");
+		//driver.hideKeyboard();
+		
+		List<WebElement> recyclerelements = driver.findElements(By.id("com.android.vending:id/suggest_text"));
+		
+		System.out.println(recyclerelements.size());
+		
+		for(int i = 0; i<recyclerelements.size() ; i++)
+		{
+			String text = recyclerelements.get(i).getText().trim();
+			
+			if(text.equals("Flipkart Online Shopping App"))
+			{
+				recyclerelements.get(i).click();
+				break;
+			}
+		}
+	}
+	
+	@Test(priority=1)
+	public void scrollToElement() throws InterruptedException
+	{
+		//String appdescription = "App: Myntra Online Shopping App10.0MB";
+		String appdescription = "Myntra Online";
+		driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(new UiSelector().descriptionContains(\""+appdescription+"\"))")).click();
+		//element.click();
+		driver.findElement(By.xpath("//android.widget.TextView[contains(@text,'Install')]")).click();
+		driver.navigate().back();
+		Thread.sleep(2000);
+		
+	}
+
+	@AfterMethod
+	public void End() throws InterruptedException 
+	{
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
---
+++ src/com/demo/googleplay/OpeningWebURL.java
@@ -0,0 +1,55 @@
+package com.demo.googleplay;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class OpeningWebURL {
+	
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() throws MalformedURLException
+	{
+		DesiredCapabilities capabilities = DesiredCapabilities.android();
+		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.CHROME);
+		capabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.ANDROID);
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"92010457a0ce2315");
+		capabilities.setCapability(MobileCapabilityType.VERSION,"5.1.1");
+
+			
+		URL url= new URL("http://127.0.0.1:4723/wd/hub");
+		driver = new AndroidDriver(url, capabilities);
+		
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("http://www.system.netsuite.com");		
+	}
+	
+	
+	@Test
+	public void getCurrentPageTitle()
+	{
+		String title = driver.getTitle();
+		System.out.println("Current page title is : "+title);
+	}
+	
+	@AfterMethod
+	public void quit()
+	{
+		driver.quit();
+	}
+}
